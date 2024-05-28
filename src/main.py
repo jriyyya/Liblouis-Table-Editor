0fcdff9b2b5f32bc488e075cf7666b5f151959c5
Riya Jain <riya1jain567@gmail.com>
2024-05-28 18:10:17 +0530

    Add remove button for table entries

--- src/main.py
+++ src/main.py
@@ -2,11 +2,34 @@ import sys
 from utils.view import clearLayout
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
 from components.UnicodeSelector import UnicodeSelector
 from components.OpcodeSelector import OpcodeSelector
 from components.ButtonTextInput import ButtonTextInput
 
 from config import WINDOW_WIDTH, WINDOW_HEIGHT 
+
+class TableEntryWidget(QWidget):
+    def __init__(self, entry, remove_callback):
+        super().__init__()
+        layout = QHBoxLayout()
+
+        self.label = QLabel(entry)
+        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        layout.addWidget(self.label)
+
+        self.remove_button = QPushButton("Remove")
+        self.remove_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.remove_button.setStyleSheet("padding: 4px; margin: 1px;")  # Reduce padding and margin
+        layout.addWidget(self.remove_button)
+
+        layout.setContentsMargins(5, 5, 5, 5)  # Remove margins around the layout
+        # layout.setSpacing(5)  # Set spacing between widgets
+
+        self.setLayout(layout)
+
+        self.remove_button.clicked.connect(lambda: remove_callback(entry))
 
 class TableManager(QWidget):
     def __init__(self):
@@ -26,7 +49,7 @@ class TableManager(QWidget):
         self.input_opcode.input.setReadOnly(True)
         self.input_opcode.input.setPlaceholderText("Select Opcode")
         self.input_opcode.button.setText("Select")
-        self.input_opcode.button.clicked.connect(lambda : self.showOpcodePopup())
+        self.input_opcode.button.clicked.connect(self.showOpcodePopup)
 
         left_panel.addWidget(self.input_opcode)
 
@@ -38,6 +61,9 @@ class TableManager(QWidget):
         add_button.clicked.connect(self.updateTable)
         left_panel.addWidget(add_button)
 
+        left_panel_widget = QWidget()
+        left_panel_widget.setLayout(left_panel)
+
         # Right panel for table display
         right_panel = QVBoxLayout()
 
@@ -47,23 +73,17 @@ class TableManager(QWidget):
         self.table_lines_list = QListWidget()
         right_panel.addWidget(self.table_lines_list)
 
-        splitter = QSplitter(Qt.Horizontal)
-        splitter.addWidget(QWidget())  # Placeholder for future expansion
-        splitter.addWidget(QWidget())  # Placeholder for future expansion
-
-        splitter.setStretchFactor(0, 1)  # Make left panel stretchable
-        splitter.setStretchFactor(1, 0)  # Make right panel non-stretchable
-
-        layout.addWidget(splitter)
-
-        left_panel_widget = QWidget()
-        left_panel_widget.setLayout(left_panel)
         right_panel_widget = QWidget()
         right_panel_widget.setLayout(right_panel)
 
-        splitter.widget(0).setLayout(left_panel.layout())
-        splitter.widget(1).setLayout(right_panel.layout())
+        # Create QSplitter and add left and right panels
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(left_panel_widget)
+        splitter.addWidget(right_panel_widget)
+        splitter.setStretchFactor(0, 1)
+        splitter.setStretchFactor(1, 1)
 
+        layout.addWidget(splitter)
         self.setLayout(layout)
 
         self.selected_opcode = None
@@ -76,7 +96,7 @@ class TableManager(QWidget):
             self.generateForm(opcode["fields"])
 
         self.popup = OpcodeSelector()
-        self.popup.on_select(lambda code : on_select(code))
+        self.popup.on_select(on_select)
         self.popup.show()
 
     def generateForm(self, fields):
@@ -95,9 +115,9 @@ class TableManager(QWidget):
                 i = inp.input
                 def open_popup():
                     self.popup = UnicodeSelector()
-                    self.popup.on_select(lambda code : i.setText(code))
+                    self.popup.on_select(lambda code: i.setText(code))
                     self.popup.show()
-                inp.button.clicked.connect(lambda : open_popup())
+                inp.button.clicked.connect(open_popup)
                 self.form.addWidget(inp)
 
             if field == "dots":
@@ -130,7 +150,15 @@ class TableManager(QWidget):
     def updateTableDisplay(self):
         self.table_lines_list.clear()
         for entry in self.table_entries:
-            self.table_lines_list.addItem(entry)
+            item = QListWidgetItem()
+            widget = TableEntryWidget(entry, self.removeEntry)
+            item.setSizeHint(widget.sizeHint())
+            self.table_lines_list.addItem(item)
+            self.table_lines_list.setItemWidget(item, widget)
+
+    def removeEntry(self, entry):
+        self.table_entries.remove(entry)
+        self.updateTableDisplay()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
