fff4ba487a94b257627e4b55efc82ab781e6909f
Riya Jain <riya1jain567@gmail.com>
2024-05-29 15:50:09 +0530

    Fix bug of clearLayout and not being able to change opcode

--- src/utils/view.py
+++ src/utils/view.py
@@ -1,3 +1,16 @@
+from PyQt5.QtWidgets import *
+
 def clearLayout(layout):
-    for i in reversed(range(layout.count())): 
-        layout.itemAt(i).widget().setParent(None)
+    for i in reversed(range(layout.count())):
+        item = layout.itemAt(i)
+        if isinstance(item, QWidgetItem):
+            widget = item.widget()
+            if widget is not None:
+                widget.setParent(None)
+            else:
+                layout.removeItem(item)
+        elif isinstance(item, QLayoutItem):
+            sub_layout = item.layout()
+            if sub_layout is not None:
+                clearLayout(sub_layout)
+                layout.removeItem(item)
