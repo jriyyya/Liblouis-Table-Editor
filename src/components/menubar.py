6a0cc3f9c21b41a42ed12b6d12e3df02fe485fae
Riya Jain <riya1jain567@gmail.com>
2024-06-28 13:33:29 +0530

    Added Icons to menubar

--- src/components/menubar.py
+++ src/components/menubar.py
@@ -1,7 +1,11 @@
 from PyQt5.QtWidgets import QMenuBar, QAction
+from PyQt5.QtGui import QIcon
+import os
 
-def create_action(parent, title, shortcut=None):
+def create_action(parent, title, icon_path=None, shortcut=None):
     action = QAction(title, parent)
+    if icon_path:
+        action.setIcon(QIcon(icon_path))
     if shortcut:
         action.setShortcut(shortcut)
     return action
@@ -18,30 +22,33 @@ def add_menu_with_actions(menubar, title, actions):
 def create_menubar(parent):
     menubar = QMenuBar(parent)
 
-    # Define menu structure
+    # Base path for icons
+    icon_base_path = os.path.join(os.path.dirname(__file__), '..', 'assets', 'icons')
+
+    # Define menu structure with icon paths
     menu_structure = {
         'File': [
-            ('New', 'Ctrl+N'),
-            ('Open', 'Ctrl+O'),
-            ('Save', 'Ctrl+S'),
-            ('Save As', 'Ctrl+Shift+S')
+            ('New', os.path.join(icon_base_path, 'new.png'), 'Ctrl+N'),
+            ('Open', os.path.join(icon_base_path, 'open.png'), 'Ctrl+O'),
+            ('Save', os.path.join(icon_base_path, 'save.png'), 'Ctrl+S'),
+            ('Save As', os.path.join(icon_base_path, 'save_as.png'), 'Ctrl+Shift+S')
         ],
         'Edit': [
-            ('Undo', 'Ctrl+Z'),
-            ('Redo', 'Ctrl+Y'),
+            ('Undo', os.path.join(icon_base_path, 'undo.png'), 'Ctrl+Z'),
+            ('Redo', os.path.join(icon_base_path, 'redo.png'), 'Ctrl+Y'),
             'separator',
-            ('Go to Entry', 'Ctrl+I'),
-            ('Find', 'Ctrl+F'),
-            ('Find and Replace', 'Ctrl+H')
+            ('Go to Entry', os.path.join(icon_base_path, 'go_to_entry.png'), 'Ctrl+I'),
+            ('Find', os.path.join(icon_base_path, 'find.png'), 'Ctrl+F'),
+            ('Find and Replace', os.path.join(icon_base_path, 'find_replace.png'), 'Ctrl+H')
         ],
         'Tools': [
-            ('Increase Font Size', 'Ctrl+]'),
-            ('Decrease Font Size', 'Ctrl+[')
+            ('Increase Font Size', os.path.join(icon_base_path, 'increase_font.png'), 'Ctrl+]'),
+            ('Decrease Font Size', os.path.join(icon_base_path, 'decrease_font.png'), 'Ctrl+[')
         ],
         'Help': [
-            ('About', None),
-            ('Report a bug', None),
-            ('User Guide', None)
+            ('About', os.path.join(icon_base_path, 'about.png'), None),
+            ('Report a bug', os.path.join(icon_base_path, 'report_bug.png'), None),
+            ('User Guide', os.path.join(icon_base_path, 'user_guide.png'), None)
         ]
     }
 
@@ -52,8 +59,8 @@ def create_menubar(parent):
             if action == 'separator':
                 action_list.append('separator')
             else:
-                title, shortcut = action
-                action_list.append(create_action(parent, title, shortcut))
+                title, icon_path, shortcut = action
+                action_list.append(create_action(parent, title, icon_path, shortcut))
         add_menu_with_actions(menubar, menu_title, action_list)
 
     return menubar
